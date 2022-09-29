2e66d31f55affff289690acb254723db1d1c09df
Danilo Lima <110578447+DaniloLima0707@users.noreply.github.com>
2022-09-29 11:45:58 -0400

    Criação da SecondActivity

--- app/src/main/java/com/example/hellowordmultitelas/MainActivity.java
+++ app/src/main/java/com/example/hellowordmultitelas/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.hellowordmultitelas;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText campoNome;
+    private Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        campoNome = findViewById(R.id.campoNome);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hellowordmultitelas/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.example.hellowordmultitelas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+}
